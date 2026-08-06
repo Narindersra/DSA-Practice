a9ae01438ba4a387fad0d290bd3c8986c6697d43
Narindersra <srawriter1@gmail.com>
2026-08-06 22:53:31 +0530

    Is rotated Array is Sorted

--- Array/Sorted_Array.java
+++ Array/Sorted_Array.java
@@ -2,10 +2,17 @@
 
 public class Sorted_Array {
     boolean isSorted(int[] arr, int n){
+        int count = 0;
         for(int i = 1; i < n; i++){
             if(arr[i] < arr[i-1]){
-                return false;
+                count++;
             }
+        }
+        if(arr[n-1] > arr[0]){
+            count++;
+        }
+        if(count > 1){
+            return false;
         }
         return true;
     }
@@ -13,7 +20,7 @@ public class Sorted_Array {
     public static void main(String[] args) {
         Sorted_Array sol = new Sorted_Array();
         int[] arr = {1, 2, 3, 4, 5};
-        int[] arr2 = {5, 4, 3, 2, 1};
+        int[] arr2 = {3, 4, 5, 1, 2};
         int n = arr.length;
         System.out.println(sol.isSorted(arr, n));
         System.out.println(sol.isSorted(arr2, n));
